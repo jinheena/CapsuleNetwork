656bc8294b69521ee3456f7f016946e939769ba0
jinheena <jinheena@gmail.com>
2018-01-05 16:45:07 +0900

    add checkpoint & test logic

---
+++ checkpoint.py
@@ -0,0 +1,102 @@
+# copy and modify from https://github.com/IBM/pytorch-seq2seq/blob/master/seq2seq/util/checkpoint.py
+from __future__ import print_function
+import os
+import time
+import shutil
+
+import torch
+import dill
+
+class Checkpoint(object):
+    """
+    The Checkpoint class manages the saving and loading of a model during training. It allows training to be suspended
+    and resumed at a later time (e.g. when running on a cluster using sequential jobs).
+    To make a checkpoint, initialize a Checkpoint object with the following args; then call that object's save() method
+    to write parameters to disk.
+    Args:
+        model : model being trained
+        optimizer (Optimizer): stores the state of the optimizer
+        epoch (int): current epoch (an epoch is a loop through the full training data)
+    Attributes:
+        CHECKPOINT_DIR_NAME (str): name of the checkpoint directory
+        TRAINER_STATE_NAME (str): name of the file storing trainer states
+        MODEL_NAME (str): name of the file storing model
+    """
+
+    CHECKPOINT_DIR_NAME = 'checkpoints'
+    TRAINER_STATE_NAME = 'trainer_states.pt'
+    MODEL_NAME = 'model.pt'
+
+    def __init__(self, model, optimizer, epoch, path=None):
+        self.model = model
+        self.optimizer = optimizer
+        self.epoch = epoch
+        self._path = path
+
+    @property
+    def path(self):
+        if self._path is None:
+            raise LookupError("The checkpoint has not been saved.")
+        return self._path
+
+    def save(self, experiment_dir):
+        """
+        Saves the current model and related training parameters into a subdirectory of the checkpoint directory.
+        The name of the subdirectory is the current local time in Y_M_D_H_M_S format.
+        Args:
+            experiment_dir (str): path to the experiment root directory
+        Returns:
+             str: path to the saved checkpoint subdirectory
+        """
+        date_time = time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime())
+
+        self._path = os.path.join(experiment_dir, self.CHECKPOINT_DIR_NAME, date_time)
+        path = self._path
+
+        if os.path.exists(path):
+            shutil.rmtree(path)
+        os.makedirs(path)
+        torch.save({'epoch': self.epoch,
+                    #'step': self.step,
+                    'optimizer': self.optimizer
+                   },
+                   os.path.join(path, self.TRAINER_STATE_NAME))
+        torch.save(self.model, os.path.join(path, self.MODEL_NAME))
+
+        return path
+
+    @classmethod
+    def load(cls, path):
+        """
+        Loads a Checkpoint object that was previously saved to disk.
+        Args:
+            path (str): path to the checkpoint subdirectory
+        Returns:
+            checkpoint (Checkpoint): checkpoint object with fields copied from those stored on disk
+        """
+        if torch.cuda.is_available():
+            resume_checkpoint = torch.load(os.path.join(path, cls.TRAINER_STATE_NAME))
+            model = torch.load(os.path.join(path, cls.MODEL_NAME))
+        else:
+            resume_checkpoint = torch.load(os.path.join(path, cls.TRAINER_STATE_NAME), map_location=lambda storage, loc: storage)
+            model = torch.load(os.path.join(path, cls.MODEL_NAME), map_location=lambda storage, loc: storage)
+
+        optimizer = resume_checkpoint['optimizer']
+        return Checkpoint(model=model,
+                          optimizer=optimizer,
+                          epoch=resume_checkpoint['epoch'],
+                          path=path)
+
+    @classmethod
+    def get_latest_checkpoint(cls, experiment_path):
+        """
+        Given the path to an experiment directory, returns the path to the last saved checkpoint's subdirectory.
+        Precondition: at least one checkpoint has been made (i.e., latest checkpoint subdirectory exists).
+        Args:
+            experiment_path (str): path to the experiment directory
+        Returns:
+             str: path to the last saved checkpoint's subdirectory
+        """
+        checkpoints_path = os.path.join(experiment_path, cls.CHECKPOINT_DIR_NAME)
+        all_times = sorted(os.listdir(checkpoints_path), reverse=True)
+        return os.path.join(checkpoints_path, all_times[0])
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import argparse
 import torch
@@ -6,76 +7,141 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 from torch.optim import Adam
 from torchvision import datasets, transforms
+import cv2
+import random
 
 from dataloader import get_mnist_data
 from network import CapsuleNetwork
-
+from checkpoint import Checkpoint
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', required=True, help='mnist')
 parser.add_argument('--batch_size', type=int, help='input batch size', default=128)
-parser.add_argument('--n_epochs', type=int, help='number of epoch', default = 30)
-parser.add_argument('--cuda', type=bool, help='enables cuda', default = True)
+parser.add_argument('--n_epochs', type=int, help='number of epoch', default=30)
+parser.add_argument('--cuda', type=bool, help='enables cuda', default=True)
+parser.add_argument('--r_lambda', type=float, help='scale down factor of the reconstruction loss', default=0.0005)
+parser.add_argument('--vis', type=bool, help='Show reconstructed output', default=False)
+parser.add_argument('--recon_with_gt', type=bool, help='Use class label for reconstrucntion', default=False)
+parser.add_argument('--save_results', type=bool, help='Save trained model and images', default=True)
+parser.add_argument('--save_folder', help='folder to save output image and model checkpoints', default='./out')
+parser.add_argument('--resume', help='resume training from the previous checkpoint', default=False)
+parser.add_argument('--is_train', help='start training ', default=True)
+
 opt = parser.parse_args()
-print opt
+if opt.save_results:
+    try:
+        os.mkdir(opt.save_folder)
+    except OSError:
+        if not os.path.isdir(opt.save_folder):
+            raise
+
+def train(epoch, model, train_loader, test_loader, optimizer):
+    model.train()
+    train_loss = 0
+    for batch_id, (data, target) in enumerate(train_loader):
+        target = torch.sparse.torch.eye(10).index_select(dim=0, index=target)
+        data, target = Variable(data), Variable(target)
+        if opt.cuda==True:
+            data, target = data.cuda(), target.cuda()
+
+        optimizer.zero_grad()
+        #output = capsule_network(data)
+        #loss = capsule_network.margin_loss(output, target)
+
+        output, mask, recon = model(data)
+        loss = model.loss(output, target, recon, data)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.data[0]
+        if batch_id % 100 == 0:
+            print "epoch : {}, train accuracy : {}".format(epoch, 
+                sum(np.argmax(mask.data.cpu().numpy(), 1) == np.argmax(target.data.cpu().numpy(), 1)) / float(opt.batch_size) )
+
+        if opt.vis==True:
+            idx = random.randint(0, data.size(0) - 1)
+            show_recon = recon[idx].data.cpu().numpy().reshape(28,28)
+            show_data = data[idx].data.cpu().numpy().reshape(28,28)
+
+            cv2.namedWindow("recon", cv2.WINDOW_NORMAL)
+            cv2.imshow("recon", np.concatenate( (show_data, show_recon), axis = 1 ) )
+            cv2.waitKey(1)
+
+            if batch_id % 100 == 0: # save reconstructed output
+                save_name = '%s/recon_%d_%d.png' % (opt.save_folder, epoch, idx)
+                cv2.imwrite(save_name, show_recon*255)
+            
+                
+    print train_loss / len(train_loader)
+
+    model.eval()
+    test_loss = 0
+    for batch_id, (data, target) in enumerate(test_loader):
+        target = torch.sparse.torch.eye(10).index_select(dim=0, index=target)
+        data, target = Variable(data), Variable(target)
+        if opt.cuda==True:
+            data, target = data.cuda(), target.cuda()
+
+        output, mask, recon = model(data)
+        loss = model.loss(output, target, recon, data)
+
+        test_loss += loss.data[0]
+        
+        if batch_id % 100 == 0:
+            print "test accuracy:", sum(np.argmax(mask.data.cpu().numpy(), 1) == 
+                                np.argmax(target.data.cpu().numpy(), 1)) / float(opt.batch_size)
+    print test_loss / len(test_loader)
+
+def test(model, test_loader):
+    latest_checkpoint_path = Checkpoint.get_latest_checkpoint(opt.save_folder)
+    resume_checkpoint = Checkpoint.load(latest_checkpoint_path)
+    model = resume_checkpoint.model
+    optimizer = resume_checkpoint.optimizer
+
+    model.eval()
+    test_loss = 0
+    for batch_id, (data, target) in enumerate(test_loader):
+        data = Variable(data)
+        if opt.cuda==True:
+            data = data.cuda()
+
+        output, mask, recon = model(data)
+
+        out_mag= torch.sqrt( (output**2).sum(2) )
+        out_mag = F.softmax(out_mag)
+        max_val, max_idx = out_mag.max(dim=1)
+        print "classification label : ", max_idx[0]
+
+        if opt.vis==True:
+            show_recon = recon[0].data.cpu().numpy().reshape(28,28)
+            show_data = data[0].data.cpu().numpy().reshape(28,28)
+
+            cv2.namedWindow("recon", cv2.WINDOW_NORMAL)
+            cv2.imshow("recon", np.concatenate( (show_data, show_recon), axis = 1 ) )
+            cv2.waitKey(1000)
 
 def main():
-   
     train_loader, test_loader = get_mnist_data('../mnist', opt.batch_size)
-
-    capsule_network = CapsuleNetwork(opt)
+    model = CapsuleNetwork(opt)
     if opt.cuda==True:
-        capsule_network = capsule_network.cuda()
-    optimizer = Adam(capsule_network.parameters())
+        model = model.cuda()
     
-    for epoch in range(opt.n_epochs):
-        print 'epoch {}'.format(epoch)
-        capsule_network.train()
-        train_loss = 0
-        for batch_id, (data, target) in enumerate(train_loader):
-            target = torch.sparse.torch.eye(10).index_select(dim=0, index=target)
-            data, target = Variable(data), Variable(target)
-            if opt.cuda==True:
-                data, taget = data.cuda(), target.cuda()
-
-            optimizer.zero_grad()
-            #output = capsule_network(data)
-            #loss = capsule_network.margin_loss(output, target)
-
-            output, recon_output = capsule_network(data)
-
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.data[0]
-            
-            # if batch_id % 100 == 0:
-            #     print  "train accuracy:", (train_loss / float(opt.batch_size))
-                #print "train accuracy:", sum(np.argmax(masked.data.cpu().numpy(), 1) == 
-                #                    np.argmax(target.data.cpu().numpy(), 1)) / float(batch_size)
-            
-        print train_loss / len(train_loader)
-            
-        # capsule_net.eval()
-        # test_loss = 0
-        # for batch_id, (data, target) in enumerate(test_loader):
-
-        #     target = torch.sparse.torch.eye(10).index_select(dim=0, index=target)
-        #     data, target = Variable(data), Variable(target)
-        #     #if USE_CUDA:
-        #     if opt.cuda==True:
-        #         data, target = data.cuda(), target.cuda()
-
-        #     output, reconstructions, masked = capsule_net(data)
-        #     loss = capsule_net.loss(data, output, target, reconstructions)
-
-        #     test_loss += loss.data[0]
-            
-        #     if batch_id % 100 == 0:
-        #         print "test accuracy:", sutrain_loaderm(np.argmax(masked.data.cpu().numpy(), 1) == 
-        #                             np.argmax(target.data.cpu().numpy(), 1)) / float(batch_size)
+    if opt.is_train==True:
+        if opt.resume==True:
+            latest_checkpoint_path = Checkpoint.get_latest_checkpoint(opt.save_folder)
+            resume_checkpoint = Checkpoint.load(latest_checkpoint_path)
+            model = resume_checkpoint.model
+            optimizer = resume_checkpoint.optimizer
+            start_epoch = resume_checkpoint.epoch + 1
+        else:
+            start_epoch = 0
+            optimizer = Adam(model.parameters())
         
-        # print test_loss / len(mnist.test_loader)
 
+        for epoch in range(start_epoch, opt.n_epochs):
+            train(epoch, model, train_loader, test_loader, optimizer)
+            Checkpoint(model=model,optimizer=optimizer, epoch=epoch).save(opt.save_folder)
+    else:
+        test(model, test_loader)
 
 if __name__ == "__main__":
     main()
