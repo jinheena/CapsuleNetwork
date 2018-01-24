4b0faae7e4a11017017ec8ee544acef8c9b96e93
jin <jhna78@gmail.com>
2018-01-24 23:22:42 +0900

    bug fix of the affinement transformation matrix

--- layers.py
+++ layers.py
@@ -27,9 +27,9 @@ def softmax(input, dim=1):
     return soft_max_nd.transpose(dim, len(input_size) - 1)
 
 class ConvLayer(nn.Module):
-    def __init__(self, in_channels=1, out_channels=256, kernel_size=9):
+    def __init__(self, in_channels=1, out_channels=256, kernel_size=9, stride=1):
         super(ConvLayer, self).__init__()
-        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=1)
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride)
            
     # x (NCWH) : 128 x 1 x 28 x 28
     # out (NCWH) : 128 x 256 x 20 x 20
@@ -38,9 +38,9 @@ class ConvLayer(nn.Module):
 
 
 class PrimaryCapsLayer(nn.Module):
-    def __init__(self, num_capsules=8, in_channels=256, out_channels=32, kernel_size=9):
+    def __init__(self, dim_capsules=8, in_channels=256, out_channels=32, kernel_size=9):
         super(PrimaryCapsLayer, self).__init__()
-        self.capsules = nn.ModuleList([nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=2, padding=0) for _ in range(num_capsules)])
+        self.capsules = nn.ModuleList([nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=2, padding=0) for _ in range(dim_capsules)])
 
     # x (NCWH) : 128 x 256 x 20 x 20  
     # out (NCWH) : 128 x 1152(=32x6x6) x 8      
@@ -55,26 +55,28 @@ class PrimaryCapsLayer(nn.Module):
         return (x_norm * x) / ( (1.0 + x_norm) * torch.sqrt(x_norm) )
 
 class DigitCapsuleLayer(nn.Module):
-    def __init__(self, opt, num_capsules=10, num_routes=32*6*6, in_channels=8, out_channels=16):
+    #def __init__(self, opt, num_capsules=10, num_routes=32*6*6, in_channels=8, out_channels=16):
+    def __init__(self, opt, num_capsules=10, num_routes=32*6*6, num_w=32, in_channels=8, out_channels=16):
         super(DigitCapsuleLayer, self).__init__()
         self.opt = opt
         self.num_capsules = num_capsules
         self.num_routes = num_routes
-        self.W = nn.Parameter(torch.randn(1, num_routes, num_capsules, out_channels, in_channels))
+        self.W = nn.Parameter(torch.randn(1, num_w, num_capsules, out_channels, in_channels))
 
 
     # batch_size = 128
     # u : 128 x 1152(=32x6x6) x 8
     # u stack : 128 x 1152 x 10 x 8 x 1
-    # W : 1 x 1152 x 10 x 16 x 8
-    # W_concat : 128 x 1152 x 10 x 16 x 8
-    # u_hat(W_concat*u_stack) : 128 x 1152 x 10 x 16 x 1
+    # W : 1 x 32 x 10 x 16 x 8  (not 1 x 1152 x 10 x 16 x 8)
+    # W_concat : 128 x 1152 x 10 x 16 x 8 (concate twice, dim 1 & dim 0)
+    # u_hat(W_concat*u_stack) : 128 x 128 x 1152 x 10 x 16 x 1
     # stack : # Concatenates sequence of tensors along a new dimension.
     # cat : Concatenates the given sequence of seq tensors in the given dimension. All tensors must either have the same shape (except in the cat dimension) or be empty.
     def forward(self, u):
         batch_size = u.size(0)
         u_stack = torch.stack([u] * self.num_capsules, dim=2).unsqueeze(4)
-        W_concat = torch.cat([self.W] * batch_size, dim=0)
+        W_concat = torch.cat([self.W] * (6*6), dim=1)
+        W_concat = torch.cat([W_concat] * batch_size, dim=0)
         u_hat = torch.matmul(W_concat, u_stack)
 
         # Routing algorithm (Procedure 1 in the paper)
